af56bedd100eb2975dd6103df0c9979ef5f11639
Darren Low <darren.jm.low@seagate.com>
2024-05-23 12:27:27 +0800

    added more

---
+++ beginer/nests.js
@@ -0,0 +1,68 @@
+{
+
+    "name": "World",
+	
+	
+    "children": 	[
+			      { "name": "US",
+				  "children":	[
+								{ "name": "CA" },
+								{ "name": "NJ" }
+							]
+			      },
+			      
+			      { "name": "INDIA",
+				  "children":	[
+								{ "name": "OR" },
+								{ "name": "TN" },
+								{ "name": "AP" }
+							]
+			      }
+			]
+
+      
+      
+      
+};
+
+
+
+	
+										world 
+								--------------------------------
+								US					INDIA
+						-----------------------		-----------------------
+					CA					NJ		OR		TN		AP
+				
+======================================================================================
+
+
+{
+	"name" 		: "1960",
+	"children"		:[
+					{ "name" 	: "male" 
+					   "children"	: [
+									{"name": "chinese", "population": "120000"},
+									{"name": "malay", "population": "120000"},
+									{"name": "indian", "population": "120000"},
+									{"name": "others", "population": "120000"}
+								  ]
+					},
+					
+					{ "name" 	: "female" 
+					   "children"	: [
+									{"name": "chinese", "population": "120000"},
+									{"name": "malay", "population": "120000"},
+									{"name": "indian", "population": "120000"},
+									{"name": "others", "population": "120000"}
+								  ]
+					}
+				]	
+}
+
+
+
+
+
+
+
